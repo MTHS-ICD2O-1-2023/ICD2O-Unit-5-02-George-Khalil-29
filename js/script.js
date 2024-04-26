787a0dd729767eb490139d117719c19717c3b5ac
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-26 03:05:17 +0000

    save the last changes

--- js/script.js
+++ js/script.js
@@ -4,17 +4,20 @@
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
 
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
 /**
  * This function displays the slider value.
  */
-function myButtonClicked() {
-  Positive = document.getElementById('Positive').checked
-  const randomNumber = Math.floor(Math.random() * 6) + 1
-  if (Positive == true) {
+function randomNumbers() {
+  
+  const optionPositive = document.getElementById('option-Positive').checked
+  
+  if (optionPositive == true) {
     document.getElementById("radio-button-value").innerHTML =
-      "<p>The random number is :</p>"
+    "<p>Value is: On</p>"
   } else {
     document.getElementById("radio-button-value").innerHTML =
-      "<p>The random number is : </p>"
+    "<p>Value is: Off</p>"
   }
 }
